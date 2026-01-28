558172ed02b4742dfbfb498fa96b1fe24855dae3
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-01-28 10:21:37 +0100

    Variables with url, and headers.

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+export const apiDadJokes = {
+  url: "https://icanhazdadjoke.com/",
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+export const apiChuckJokes = {
+  url: "https://api.chucknorris.io/jokes/random",
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+export const apiWeather = {
+  url: "https://api.open-meteo.com/v1/forecast",
+};
